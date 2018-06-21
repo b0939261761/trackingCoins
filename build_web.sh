17419f000d771843f611cbc616b6d8d0514a5383
Bogdan <bogdan_uman@meta.ua>
2018-06-21 14:17:27 +0300

    travis

--- build_web.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set execute permission on your script
-# chmod +x build_web.sh
-
-docker run --rm --name web --volume "$(pwd)/trackingCoinsVue:/app" -it node:latest bash -c "cd /app && npm install --global npm@latest && npm install && npm run build"
-
